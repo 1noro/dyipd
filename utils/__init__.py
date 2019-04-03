6535d09bc43e1cfc8b5a48e8ac186620098b5c76
boot1110001 <boot111.0001@gmail.com>
2019-04-03 11:41:48 +0200

    lala

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,13 +2,44 @@
 # by inoro
 
 ### IMPORTS ####################################################################
+from optparse import OptionParser
 
 ### EDITABLE VARIABLES #########################################################
+
 
 ### AUTOMATIC VARIABLES ########################################################
 
 ### NON EDITABLE VARIABLES #####################################################
+verbose = False
+background = False
+sendmail = False
 
 ### FUNCTIONS ##################################################################
 
 ### MAIN #######################################################################
+def main():
+    # --- Parameters -----------------------------------------------------------
+    parser = OptionParser()
+    parser.add_option(
+        "-v", "--verbose", dest="verbose",
+        action="store_true", default=False,
+        help="print status messages to stdout.")
+    parser.add_option(
+        "-b", "--background", dest="background",
+        action="store_true", default=False,
+        help="starts dypd in background mode.")
+    parser.add_option(
+        "-m", "--sendmail", dest="sendmail",
+        action="store_true", default=False,
+        help="send a mail when dynamic ip changes.")
+
+    (options, args) = parser.parse_args()
+
+    # --- verbose
+    verbose = options.verbose
+    # --- background
+    background = options.background
+    # --- sendmail
+    sendmail = options.sendmail
+
+    print(verbose, background, sendmail)
